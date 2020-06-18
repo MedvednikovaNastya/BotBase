368f069159d79009c6e98e22e8d721a3980f97a7
Bogdan-1-1-1 <bogdanbatanik@mail.ru>
2020-06-18 15:10:30 +0300

    commit with sprites

--- Main.cpp
+++ Main.cpp
@@ -3,10 +3,12 @@
 #include <ViZDoom.h>
 
 
-std::string path = "C:/Users/Анастасия/mine/Сессия/practice/practice/vizdoom";
+std::string path = "C:/practice/practice/vizdoom";
 auto game = std::make_unique<vizdoom::DoomGame>();
 const unsigned int sleepTime = 1000 / vizdoom::DEFAULT_TICRATE;
 auto screenBuff = cv::Mat(480, 640, CV_8UC3);
+const char* filename = "./sprites/Enemies/heada1.png";
+
 
 void RunTask1(int episodes)
 {
@@ -21,8 +23,7 @@ void RunTask1(int episodes)
     }
 
     auto greyscale = cv::Mat(480, 640, CV_8UC1);
-
-    std::vector<double> action;
+    std::vector<std::vector<double>> action = { {1, 0, 0}, {0, 1, 0}, {0, 0, 1} };
 
     for (auto i = 0; i < episodes; i++)
     {
@@ -35,17 +36,34 @@ void RunTask1(int episodes)
 
             std::memcpy(screenBuff.data, gamestate->screenBuffer->data(), gamestate->screenBuffer->size());
 
-            cv::extractChannel(screenBuff, greyscale, 2);
+            cv::Mat img = screenBuff;
+            cv::Mat temp1 = cv::imread(filename);
+            cv::Mat result;
 
-            cv::threshold(greyscale, greyscale, 180, 255, cv::THRESH_BINARY);
+            int result_cols = img.cols - temp1.cols + 1;
+            int result_rows = img.rows - temp1.rows + 1;
 
-            cv::imshow("Output Window", greyscale);
+            result.create(result_rows, result_cols, CV_32FC1);
+            matchTemplate(img, temp1, result, 4);
+            normalize(result, result, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
 
-            double reward = game->makeAction({ 0, 0, 1 });
+            double minval; double maxval; cv::Point minLoc; cv::Point maxLoc;
+            minMaxLoc(result, &minval, &maxval, &minLoc, &maxLoc, cv::Mat());
+            rectangle(img, maxLoc, cv::Point(maxLoc.x + temp1.cols, maxLoc.y + temp1.rows), cv::Scalar::all(255), 2, 8, 0);
+            rectangle(result, minLoc, cv::Point(minLoc.x + temp1.cols, minLoc.y + temp1.rows), cv::Scalar::all(255), 2, 8, 0);
 
+           // cv::threshold(img, img, 254, 255, CV_THRESH_BINARY);
+            
+            //img.at<uchar>(240, 320) = 120;
+            //std::cout << img.cols << " " << img.rows;
 
-            std::cout << reward << " ";
+            cv::imshow("Origin", img);
+            cv::imshow("Result", result);
+
+            game->makeAction({0, 1, 0});
+
             cv::waitKey(sleepTime);
+
         }
 
         std::cout << std::endl << game->getTotalReward() << std::endl;
@@ -58,9 +76,7 @@ int main()
     game->setViZDoomPath(path + "/vizdoom.exe");
     game->setDoomGamePath(path + "/freedoom2.wad");
 
-    cv::namedWindow("Control Window", cv::WINDOW_AUTOSIZE);
-
-    auto episodes = 1;
+    auto episodes = 10;
 
 
     RunTask1(episodes);
