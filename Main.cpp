ade373eddfd6bc5c9956b687cf44668db00f1dfb
Bogdan-1-1-1 <bogdanbatanik@mail.ru>
2020-06-25 20:40:10 +0300

    7-th exe (middle result)

--- Main.cpp
+++ Main.cpp
@@ -10,9 +10,11 @@ const unsigned int sleepTime = 1000 / vizdoom::DEFAULT_TICRATE;
 auto screenBuff = cv::Mat(480, 640, CV_8UC3);
 const char* headaFront = "./sprites/Enemies/heada1.png";
 const char* headaSide = "./sprites/Enemies/heada2a8.png";
+const char* worm = "./sprites/Enemies/sarga1.png";
+const char* warrior = "./sprites/Enemies/cposa1.png";
 const char* medikit = "./sprites/Pickups/media0.png";
 const char* akainu = "./sprites/Enemies/akainu.png";
-std::vector<std::vector<double>> action = { {1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 1} , {0, 0, 0, 0} };
+std::vector<std::vector<double>> action = { {1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0},  {0, 0, 0, 1} , {0, 0, 0, 0} };
 int total = 0;
 
 int lengOfLine(int x1, int y1, int x2, int y2) {
@@ -233,7 +235,7 @@ void runTask2vol2(int episodes) {
         std::cout << std::endl << game->getTotalReward() << std::endl;
     }
 }
-void runTask3(int episodes) { //пока только шаблон, можно даже не запускать
+void runTask3(int episodes) {
     try
     {
         game->loadConfig(path + "/scenarios/task3.cfg");
@@ -397,7 +399,7 @@ void runTask5(int episodes) {
                 }
             }
 
-            act = 3;
+            act = 4;
             if (whiteX < 320) act = 1;
             else if (whiteX > 320) act = 0;
           
@@ -417,6 +419,185 @@ void runTask5(int episodes) {
         std::cout << std::endl << game->getEpisodeTime() << std::endl;
     }
 }
+void runTask7(int episodes) {
+    try {
+        game->loadConfig(path + "/scenarios/task7.cfg");
+        game->init();
+    }
+    catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    cv::namedWindow("Result", CV_WINDOW_AUTOSIZE);
+
+    IplImage* data;
+    IplImage* grey = cvCreateImage(cv::Size(640, 480), IPL_DEPTH_8U, 1);
+    IplImage* result = cvCreateImage(cv::Size(640, 480), IPL_DEPTH_8U, 1);
+    cv::Mat resultMat;
+    int whiteX, act;
+
+
+    for (auto i = 0; i < episodes; i++)
+    {
+        game->newEpisode();
+        std::cout << "Episode #" << i + 1 << std::endl;
+
+        while (!game->isEpisodeFinished())
+        {
+            const auto& gamestate = game->getState();
+
+            std::memcpy(screenBuff.data, gamestate->screenBuffer->data(), gamestate->screenBuffer->size());
+
+            data = (&(IplImage)screenBuff);
+            cvCvtColor(data, grey, CV_RGB2GRAY);
+            cvCanny(grey, result, 200, 255, 3);
+
+            resultMat = cv::cvarrToMat(result);
+
+            whiteX = -1;
+            for (int k = 310; k < 330; k++) {
+                if (resultMat.at<char>(230, k) < 0) {
+                    whiteX = k;
+                    break;
+                }
+            }
+
+            act = 1;
+            if (whiteX != -1) act = 3;
+
+            line(resultMat, cvPoint(0, 230), cvPoint(639, 230), cvScalar(255), 2);
+
+            game->makeAction(action[act]);
+
+            cv::imshow("Result", resultMat);
+
+            cv::waitKey(sleepTime);
+
+        }
+        total += game->getEpisodeTime();
+        std::cout << std::endl << game->getEpisodeTime() << std::endl;
+    }
+
+}
+void runTask9(int episodes) {
+    try
+    {
+        game->loadConfig(path + "/scenarios/task9.cfg");
+        game->init();
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    cv::namedWindow("Result", CV_WINDOW_AUTOSIZE);
+
+    IplImage* data;
+    IplImage* grey = cvCreateImage(cv::Size(640, 480), IPL_DEPTH_8U, 1);
+    IplImage* result = cvCreateImage(cv::Size(640, 480), IPL_DEPTH_8U, 1);
+    cv::Mat resultMat;
+
+    for (auto i = 0; i < episodes; i++)
+    {
+        game->newEpisode();
+        std::cout << "Episode #" << i + 1 << std::endl;
+
+        int whiteXl = 0, whiteXr = 639;
+        bool lch = true, rch = true;
+        int triangle = 0;
+        int rotNuml = 0, rotNumr = 0;
+
+        while (!game->isEpisodeFinished())
+        {
+            const auto& gamestate = game->getState();
+
+            std::memcpy(screenBuff.data, gamestate->screenBuffer->data(), gamestate->screenBuffer->size());
+
+            data = (&(IplImage)screenBuff);
+
+            cvCvtColor(data, grey, CV_RGB2GRAY);
+            cvCanny(grey, result, 240, 255, 3);
+
+            resultMat = cv::cvarrToMat(result);
+
+            if (!triangle) {
+                for (int k = 50; k < 270; k++) {
+                    for (int k1 = 180; k1 < 240; k1++) {
+                        if (resultMat.at<char>(k1, k) != 0) {
+                            triangle++;
+                            lch = false;
+                            break;
+                        }
+                    }
+                    if (!lch) break;
+                }
+                for (int k = 589; k > 370; k--) {
+                    for (int k1 = 180; k1 < 240; k1++) {
+                        if (resultMat.at<char>(k1, k) != 0) {
+                            triangle++;
+                            rch = false;
+                            break;
+                        }
+                    }
+                    if (!rch) break;
+                }
+            }
+
+            whiteXl = 0, whiteXr = 639;
+            if (!rch) {
+                for (int k = 589; k > 320; k--) {
+                    for (int k1 = 180; k1 < 240; k1++) {
+                        if (resultMat.at<char>(k1, k) != 0) {
+                            whiteXr = k;
+                            std::wcout << k << "\n";
+                            break;
+                        }
+                    }
+                    if (whiteXr < 639) break;
+                }
+                if (whiteXr > 345) {
+                    game->makeAction({ 0, 0, 0, 0, 0, 1, 0, 0 });
+                    rotNuml++;
+                }
+                else {
+                    game->makeAction({ 0, 0, 0, 0, 0, 0, 0, 1 });
+                    rch = true;
+                    triangle--;
+                }
+            } else 
+            if (!lch) {
+                for (int k = 50; k < 320; k++) {
+                    for (int k1 = 180; k1 < 240; k1++) {
+                        if (resultMat.at<char>(k1, k) != 0) {
+                            whiteXl = k;
+                            break;
+                        }
+                        if (whiteXl > 0) break;
+                    }
+                }
+                if (whiteXl < 295) {
+                    game->makeAction({ 0, 0, 0, 0, 1, 0, 0, 0 });
+                }
+                else {
+                    game->makeAction({ 0, 0, 0, 0, 0, 0, 0, 1 });
+                    lch = true;
+                    triangle--;
+                }
+
+            } else game->makeAction({ 0, 0, 1, 0, 0, 0, 0, 0 }); 
+            
+            line(resultMat, cv::Point(0, 250), cv::Point(639, 250), cv::Scalar(255, 255, 255), 2);
+            line(resultMat, cv::Point(589, 180), cv::Point(320, 240), cv::Scalar(255, 255, 255), 2);
+            line(resultMat, cv::Point(50, 180), cv::Point(320, 240), cv::Scalar(255, 255, 255), 2);
+            cv::imshow("Result", resultMat);
+
+            cv::waitKey(sleepTime);
+
+        }
+        total += game->getTotalReward();
+        std::cout << std::endl << game->getTotalReward() << std::endl;
+    }
+}
 
 int main()
 {
@@ -425,7 +606,7 @@ int main()
 
     auto episodes = 10;
 
-    runTask5(episodes);
+    runTask9(episodes);
     std::cout << "Total Reward (or time): " << float(total) / episodes;
 
     game->close();
